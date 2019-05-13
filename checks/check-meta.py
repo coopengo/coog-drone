73df3218d0cfe326967def9845b304d2ebac820e
MaxTeiger <max.teiger@gmail.com>
2019-05-13 10:00:35 +0200

    flake8 et simplification du code

--- checks/check-meta.py
+++ checks/check-meta.py
@@ -6,7 +6,6 @@ import os
 import re
 import sys
 import requests
-from colored import fg, bg, attr
 import base64
 
 # Retrieve from the environment variables usefull infos
@@ -26,31 +25,42 @@ RM_TOKEN = os.environ['REDMINE_TOKEN']
 RM_HEADERS = {'X-Redmine-API-Key': RM_TOKEN}
 
 # compile regular expression for title, body, changelog
-title_regexp = re.compile('\w+: .+')
-body_regexp = re.compile('.*(fix|ref) #(\d+)', re.M | re.I | re.S)
-changelog_regexp = re.compile('\* (BUG|FEA|OTH)#(\d+)')
+title_regexp = re.compile(r'\w+: .+')
+body_regexp = re.compile(r'.*(fix|ref) #(\d+)', re.M | re.I | re.S)
+changelog_regexp = re.compile(r'\* (BUG|FEA|OTH)#(\d+)')
 
-bugStr = ("## \[title_en\]((.|\n)*)"
-          "## \[title_fr\]((.|\n)*)"
-          "### \[repro_fr\]((.|\n)*)"
-          "### \[correction_fr\]((.|\n)*)"
-          "\[parametrage_fr\]((.|\n)*)"
-          "### \[scripts_fr\]((.|\n)*)"
-          "## \[business_modules\]((.|\n)*)"
-          "## \[original_description\]((.|\n)*$)")
+# pattern if bug
+# grp1 = title_en, grp3 = title_fr,
+# grp5 = Scenario de reproduction,
+# grp7 = correction,
+# grp9 = parametrages_fr,
+# grp11 = scripts_fr,
+# grp13 = business modules,
+# grp15 = original description
 
-featureStr = ("## \[title_en\]((.|\n)*)"
-              "## \[title_fr\]((.|\n)*)"
-              "### \[parametrage_fr\]((.|\n)*)"
-              "### \[scripts_fr\]((.|\n)*)"
-              "## \[business_modules\]((.|\n)*)"
-              "## \[original_description\]((.|\n)*$)")
+bugStr = (r"## \[title_en\]((.|\n)*)"
+          r"## \[title_fr\]((.|\n)*)"
+          r"### \[repro_fr\]((.|\n)*)"
+          r"### \[correction_fr\]((.|\n)*)"
+          r"\[parametrage_fr\]((.|\n)*)"
+          r"### \[scripts_fr\]((.|\n)*)"
+          r"## \[business_modules\]((.|\n)*)"
+          r"## \[original_description\]((.|\n)*$)")
+
+# pattern if feature
+# grp1 = title_en, grp3 = title_fr, grp5 = parametrages_fr,
+# grp7 = scripts_fr, grp9 = business modules, grp11 = original description
+featureStr = (r"## \[title_en\]((.|\n)*)"
+              r"## \[title_fr\]((.|\n)*)"
+              r"### \[parametrage_fr\]((.|\n)*)"
+              r"### \[scripts_fr\]((.|\n)*)"
+              r"## \[business_modules\]((.|\n)*)"
+              r"## \[original_description\]((.|\n)*$)")
 
 bug_regexp = re.compile(bugStr)
 feature_regexp = re.compile(featureStr)
 
-minimum_regexp = re.compile('[\s\S]{15,}')
-
+minimum_regexp = re.compile(r'[\s\S]{15,}')
 
 # dict containing tracker name values
 rm_trackers = {1: 'bug', 2: 'fea'}
@@ -122,23 +132,18 @@ def get_gh_files():
     gh_files = requestInJson(url, headers=GH_HEADERS)
     for f in gh_files:
         if str(rm_issue) + ".md" in f['filename']:
-            print(f['filename'] +" added to the queue...")
+            print(f['filename'] + " added to the queue...")
             ghIssueFiles[f['filename']] = f['contents_url']
             isHere = True
 
-    if isHere:    
+    if isHere:
         return gh_files
 
     else:
-        print("content :'+fg('red') + 'ko' +attr(0) +':{}.md".format(rm_issue))
+        print("content :ko:{}.md".format(rm_issue))
         sys.exit(1)
 
 
-
-# Labels check, retrieve the type of the issue (if it is a feature or a bug)
-# define :
-# rm_issue_type
-# check run order : 1st
 def check_labels():
 
     global rm_issue_type
@@ -146,14 +151,8 @@ def check_labels():
         rm_issue_type = 'fea'
     elif 'bug' in gh_labels:
         rm_issue_type = 'bug'
-    print('labels  :'+fg('green') + 'ok' + attr(0))
+    print('labels  :ok')
     return True
-
-# check the title of the pull request (if it match the regex above
-# or if the title can be bypassed)
-# define :
-# nothing just check title
-# check run order : 2nd
 
 
 def check_title():
@@ -162,18 +161,11 @@ def check_title():
         print('title   :bypass')
     else:
         if title_regexp.match(gh_pull['title']):
-            print('title   :'+fg('green') + 'ok' +attr(0))
+            print('title   :ok')
         else:
             ok = False
-            print('title   :'+fg('red') + 'ko' +attr(0) )
+            print('title   :ko')
     return ok
-
-# Check the body of the pull request
-# if the body of the pull request match the
-# regex above,
-# define :
-# rm_issue
-# check run order : 3rd
 
 
 def check_body():
@@ -192,34 +184,24 @@ def check_body():
             print(('body    :ko:issue:{}-{}'.format(issue, rm_issue)))
         else:
             # set rm_issue
-            print(('body    :'+fg('green') + 'ok' +attr(0) +':issue:{}'.format(issue)))
+            print(('body    :ok:issue:{}'.format(issue)))
             rm_issue = issue
 
     # if it doesn't match
     else:
         ok = False
-        print('body    :'+fg('red') + 'ko' +attr(0) )
+        print('body    :ko')
     return ok
-
-# Check if files are present and if the content match
-
-# pattern if feature : ## \[title_en\]((.|\n)*)## \[title_fr\]((.|\n)*)### \[parametrage_fr\]((.|\n)*)### \[scripts_fr\]((.|\n)*)## \[business_modules\]((.|\n)*)## \[original_description\]((.|\n)*$)
-# grp1 = title_en, grp3 = title_fr, grp5 = parametrages_fr, grp7 = scripts_fr, grp9 = business modules, grp11 = original description
-
-# pattern if bug : ## \[title_en\]((.|\n)*)## \[title_fr\]((.|\n)*)### \[repro_fr\]((.|\n)*)### \[correction_fr\]((.|\n)*)\[parametrage_fr\]((.|\n)*)### \[scripts_fr\]((.|\n)*)## \[business_modules\]((.|\n)*)## \[original_description\]((.|\n)*$)
-# grp1 = title_en, grp3 = title_fr, grp5 = Scenario de reproduction, grp7 = correction, grp9 = parametrages_fr, grp11 = scripts_fr, grp13 = business modules, grp15 = original description
 
 
 def check_content_mdfiles():
     ok = True
-    gh_files = get_gh_files()
+    get_gh_files()
 
     for name, content in ghIssueFiles.items():
         if str(rm_issue) in name:
             r = requestInJson(content, headers=GH_HEADERS)
             fileContent = base64.b64decode(r['content'])
-
-
 
     if real_issue_type == 'fea':
         m = feature_regexp.match(fileContent.decode('utf8'))
@@ -227,40 +209,40 @@ def check_content_mdfiles():
         # if the file match the pattern, tags are respected
         if m:
 
-            print('content :'+fg('green') + 'ok' +attr(0) +':fea')
+            print('content :ok:fea')
             title_en = m.group(1).replace("(required)", "")
             title_fr = m.group(3).replace("(required)", "")
-            parametrage_fr = m.group(5).replace(
-                "<Paramétrage (éventuel) à faire>", "")
-            scripts_fr = m.group(7).replace("<Scripts à passer>", "")
+            # parametrage_fr = m.group(5).replace(
+            #     "<Paramétrage (éventuel) à faire>", "")
+            # scripts_fr = m.group(7).replace("<Scripts à passer>", "")
             business_modules = m.group(9)
             original_description = m.group(11).replace(
                 "(required / automatic)", "")
 
             if not minimum_regexp.match(title_en):
                 ok = False
-                print('content :'+fg('red') + 'ko' +attr(0) +':title not specified')
+                print('content :ko:title not specified')
             else:
-                print('content :'+fg('green') + 'ok' +attr(0) +':title_en')
+                print('content :ok:title_en')
 
             if not minimum_regexp.match(title_fr):
                 ok = False
-                print('content :'+fg('red') + 'ko' +attr(0) +':title not specified')
+                print('content :ko:title not specified')
             else:
-                print('content :'+fg('green') + 'ok' +attr(0) +':title_fr')
+                print('content :ok:title_fr')
             if not minimum_regexp.match(business_modules):
                 ok = False
-                print('content :'+fg('red') + 'ko' +attr(0) +':business_modules not specified')
+                print('content :ko:business_modules not specified')
             else:
-                print('content :'+fg('green') + 'ok' +attr(0) +':business_modules')
+                print('content :ok:business_modules')
             if not minimum_regexp.match(original_description):
                 ok = False
-                print('content :'+fg('red') + 'ko' +attr(0) +':original_description')
+                print('content :ko:original_description')
             else:
-                print('content :'+fg('green') + 'ok' +attr(0) +':original_description')
+                print('content :ok:original_description')
 
         else:
-            print('content :'+fg('red') + 'ko' +attr(0) +':tags not respected')
+            print('content :ko:tags not respected')
             ok = False
 
     elif real_issue_type == 'bug':
@@ -268,47 +250,48 @@ def check_content_mdfiles():
 
         # if the file match the pattern, tags are respected
         if m:
-            print('content :'+fg('green') + 'ok' +attr(0) +':bug')
+            print('content :ok:bug')
             title_en = m.group(1).replace("(required)", "")
             title_fr = m.group(3).replace("(required)", "")
-            parametrage_fr = m.group(9).replace(
-                "<Paramétrage (éventuel) à faire>", "")
-            scripts_fr = m.group(11).replace("<Scripts à passer>", "")
+            # parametrage_fr = m.group(9).replace(
+            #     "<Paramétrage (éventuel) à faire>", "")
+            # scripts_fr = m.group(11).replace("<Scripts à passer>", "")
             business_modules = m.group(13)
             original_description = m.group(15).replace(
                 "(required / automatic)", "")
 
             if not minimum_regexp.match(title_en):
                 ok = False
-                print('content :'+fg('red') + 'ko' +attr(0) +':title not specified')
+                print('content :ko:title not specified')
             else:
-                print('content :'+fg('green') + 'ok' +attr(0) +':title_en')
+                print('content :ok:title_en')
 
             if not minimum_regexp.match(title_fr):
                 ok = False
-                print('content :'+fg('red') + 'ko' +attr(0) +':title not specified')
+                print('content :ko:title not specified')
             else:
-                print('content :'+fg('green') + 'ok' +attr(0) +':title_fr')
+                print('content :ok:title_fr')
             if not minimum_regexp.match(business_modules):
                 ok = False
-                print('content :'+fg('red') + 'ko' +attr(0) +':business_modules not specified')
+                print('content :ko:business_modules not specified')
             else:
-                print('content :'+fg('green') + 'ok' +attr(0) +':business_modules')
+                print('content :ok:business_modules')
             if not minimum_regexp.match(original_description):
                 ok = False
-                print('content :'+fg('red') + 'ko' +attr(0) +':original_description')
+                print('content :ko:original_description')
             else:
-                print('content :'+fg('green') + 'ok' +attr(0) +':original_description')
+                print('content :ok:original_description')
 
         else:
-            print('content :'+fg('red') + 'ko' +attr(0) +':tags not respected')
+            print('content :ko:tags not respected')
             ok = False
 
     if ok:
-        print('content :'+fg('green') + 'ok' +attr(0))
+        print('content :ok')
     else:
-        print('content :'+fg('red') + 'ko' +attr(0) )
+        print('content :ko')
     return ok
+
 
 def _check_content_changelog_line(label, line):
     ok = True
@@ -333,10 +316,11 @@ def _check_content_changelog_line(label, line):
                     label, issue, rm_issue)))
             else:
                 rm_issue = issue
-                print(('content:ok:changelog:{}:issue:{}'.format(label, issue)))
+                print(('content:ok:changelog:{}:issue:{}'.format(
+                    label, issue)))
         else:
             print(('content:ok:changelog:{}:issue_type:{}'.format(
-                    label, issue_type)))
+                label, issue_type)))
     else:
         ok = False
         print(('content:ko:changelog:{}:{}'.format(label, line)))
@@ -369,9 +353,10 @@ def check_content():
                         changelog['filename'])))
         else:
             ok = False
-            print('content:' +fg('red') +'ko' +attr(0) +':changelog')
+            print('content:ko:changelog')
     return ok
-# 
+
+
 def check_redmine():
     ok = True
     global rm_issue, rm_issue_type, real_issue_type
@@ -382,7 +367,7 @@ def check_redmine():
         url = RM_URL.format(issue=rm_issue)
         issue = requestInJson(url, headers=RM_HEADERS)['issue']
 
-        print(('redmine :'+fg('green') + 'ok' +attr(0) +':issue:{}'.format(issue['id'])))
+        print(('redmine:ko:issue:{}'.format(issue['id'])))
 
         # if issue type is defined (issue type == feature or bug)
         if rm_issue_type:
@@ -392,31 +377,31 @@ def check_redmine():
 
             #
             if issue_type == rm_issue_type:
-                print(('redmine :'+fg('green') + 'ok' +attr(0) +':issue_type:{}'.format(issue_type)))
+                print(('redmine :ok:issue_type:{}'.format(issue_type)))
             else:
                 ok = False
-                print(('redmine :'+fg('red') + 'ko' +attr(0) +':issue_type:{}-{}'.format(
+                print(('redmine :ko:issue_type:{}-{}'.format(
                     issue_type, rm_issue_type)))
 
             issue_project = issue['project']['id']
             if issue_project in issues_projects:
-                print(('redmine :'+fg('green') + 'ok' +attr(0) +':issue_project:{}'.format(
+                print(('redmine :ok:issue_project:{}'.format(
                     issue['project']['name'])))
             else:
                 ok = False
-                print(('redmine :'+fg('red') + 'ko' +attr(0) +':issue_project:{}'.format(
+                print(('redmine :ko:issue_project:{}'.format(
                     issue['project']['name'])))
 
         # if issue type is not defined
         else:
             ok = False
-            print('redmine :'+fg('red') + 'ko' +attr(0) +':issue_type:empty')
+            print('redmine :ko:issue_type:empty')
 
     # if redmine issue is not defined
     elif rm_issue is None:
         # No problem if issue is #0000
         ok = False
-        print('redmine :'+fg('red') + 'ko' +attr(0) +':issue:empty')
+        print('redmine :ko:issue:empty')
 
     return ok
 
